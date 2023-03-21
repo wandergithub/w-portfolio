637a2806015c4a5c0ce0889472d1721bb101b8e2
wandergithub <wandergonzalez0@hotmail.com>
2023-03-21 12:28:40 -0400

    Add Sidebar component, delete boilerplate files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import React, { useState } from "react";
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
+  const [isopen, setisopen] = useState(false);
+  const toggle = () => {
+    setisopen(!isopen);
+  };
+
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <Navbar toggle={toggle} />
+      <Sidebar isopen={isopen} toggle={toggle} />
+    </>
   );
 }
 
