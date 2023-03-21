6c13a7501f2c5814a5fd6efb4dedd513e6ac9cb8
wandergithub <wandergonzalez0@hotmail.com>
2023-03-21 11:40:07 -0400

    Revert "Construct responsive navbar using tilwind, css modules and hooks"
    This reverts commit eca749ca0c229655e5cc86ea15bc48526127de4b.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-import React from "react";
-import { FaDev } from "react-icons/fa";
-import styles from "./App.module.css";
-import useNavigation from "./hooks/useNavigation";
-import navigationData from "./data/navigation";
-
-import Navbar from "./components/Navbar";
-import Tabbar from "./components/Tabbar";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const { currentRoute, setCurrentRoute } = useNavigation();
-
   return (
-    <div className={styles.container}>
-      <Navbar
-        navigationData={navigationData}
-        currentRoute={currentRoute}
-        setCurrentRoute={setCurrentRoute}
-      />
-      <Tabbar
-        navigationData={navigationData}
-        currentRoute={currentRoute}
-        setCurrentRoute={setCurrentRoute}
-      />
-      <div className={styles.devLogo}>
-        <FaDev />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
