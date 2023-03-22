e3a62f257971b152ceec8f6d566867b0f269a303
wandergithub <wandergonzalez0@hotmail.com>
2023-03-21 20:22:50 -0400

    Add layout, about me page component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,27 @@
+import "./About.scss";
+import me from "./assets/me.webp";
 const About = () => {
-    return (
-        <>
-            <h1>About me page</h1>
-        </>
-    )
+  return (
+    <div className="about">
+      <div className="me">
+        <img src={me} />
+      </div>
+      <div>
+        <h1 className="title-1 text-uppercase">
+          i'm Wander gonzalez.
+          <br />
+          <span>Web Developer</span>
+        </h1>
+        <p className="text-p">
+          I am a Full-Stack web developer with a passion for computer science. I
+          graduated from college with a degree in Mechatronics, where I
+          developed an interest in electronics and technology. In my free time,
+          I enjoy reading and exercising.
+        </p>
+        <button> More About Me</button>
+      </div>
+    </div>
+  );
 };
 
 export default About;
