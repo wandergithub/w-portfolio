7ef1799b5a668c52ce73b28588b6201329b4ca4e
wandergithub <wandergonzalez0@hotmail.com>
2023-03-22 10:24:49 -0400

    Add and style my works title

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,9 +1,21 @@
+import './Work.scss';
+
 const Work = () => {
-    return (
-        <>
-            <h1>Work page</h1>
-        </>
-    )
+  return (
+    <>
+      <div
+        class="title-section text-left text-sm-center aos-init aos-animate"
+        data-aos="fade-up"
+        data-aos-duration="1200"
+      >
+        <h1>
+          my
+          <span>portfolio</span>
+        </h1>
+        <span class="title-bg">works</span>
+      </div>
+    </>
+  );
 };
 
 export default Work;
