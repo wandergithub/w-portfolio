8698bc8e001db33beaf43b25000f7f7b3c18a5ec
wandergithub <wandergonzalez0@hotmail.com>
2023-03-23 10:26:34 -0400

    Add social media react icons and colors

--- src/components/FloatingSocial/FloatingSocial.js
+++ src/components/FloatingSocial/FloatingSocial.js
@@ -1,18 +1,23 @@
+import "./FloatingSocial.scss";
 import { FaLinkedinIn } from "react-icons/fa";
 import { BsGithub } from "react-icons/bs";
 import { SiMedium } from "react-icons/si";
+import { GrInstagram } from "react-icons/gr";
 
 const FloatingSocial = () => {
   return (
-    <div>
-      <a>
-        <FaLinkedinIn />
+    <div className="icons-container">
+      <a style={{ backgroundColor: "#0e76a8" }}>
+        <FaLinkedinIn size={"2em"} color={"white"} />
       </a>
-      <a>
-        <BsGithub />
+      <a style={{ backgroundColor: "#4e545a" }}>
+        <BsGithub size={"2em"} color={"white"} />
       </a>
-      <a>
-        <SiMedium />
+      <a style={{ backgroundColor: "black" }}>
+        <SiMedium size={"2em"} color={"white"} />
+      </a>
+      <a className="ig">
+        <GrInstagram size={"2em"} color={"white"} />
       </a>
     </div>
   );
