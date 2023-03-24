a6f21041ce64357097bee0ab0b8fd0c5ffc2e69a
wandergithub <wandergonzalez0@hotmail.com>
2023-03-23 21:34:26 -0400

    Add social media icons and links

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,11 @@
 import Button from "../shared/Button/Button";
 import { RiMailSendLine } from "react-icons/ri";
-import { FaMap } from "react-icons/fa";
+import { FaMap, FaLinkedinIn } from "react-icons/fa";
 import { GoMailRead } from "react-icons/go";
 import { SlCallIn } from "react-icons/sl";
+import { BsGithub } from "react-icons/bs";
+import { SiMedium } from "react-icons/si";
+import { GrInstagram, GrTwitter } from "react-icons/gr";
 
 const Contact = () => {
   return (
@@ -25,31 +28,62 @@ const Contact = () => {
           projects, creative ideas or opportunities to be part of your visions.
         </p>
         <div>
-            <FaMap size={"2em"} />
-            <div>
-                <h4>
-                    address point
-                </h4>
-                <p>123 Stree New York City , United States Of America 750065.</p>
-            </div>
+          <FaMap size={"2em"} />
+          <div>
+            <h4>address point</h4>
+            <p>123 Stree New York City , United States Of America 750065.</p>
+          </div>
         </div>
         <div>
-            <GoMailRead size={"2em"} />
-            <div>
-                <h4>
-                    mail me
-                </h4>
-                <p>wandergonzalez0@hotmail.com</p>
-            </div>
+          <GoMailRead size={"2em"} />
+          <div>
+            <h4>mail me</h4>
+            <p>wandergonzalez0@hotmail.com</p>
+          </div>
         </div>
         <div>
-            <SlCallIn size={"2em"} />
-            <div>
-                <h4>
-                    call me
-                </h4>
-                <p>+1 849 631 6566</p>
-            </div>
+          <SlCallIn size={"2em"} />
+          <div>
+            <h4>call me</h4>
+            <p>+1 849 631 6566</p>
+          </div>
+        </div>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/wander-gonzalez/"
+            target="_blank"
+            style={{ backgroundColor: "#2b2a2a" }}
+          >
+            <FaLinkedinIn size={"2em"} color={"white"} />
+          </a>
+          <a
+            href="https://github.com/wandergithub"
+            target="_blank"
+            style={{ backgroundColor: "#2b2a2a" }}
+          >
+            <BsGithub size={"2em"} color={"white"} />
+          </a>
+          <a
+            href="https://medium.com/@wandergonzalez0"
+            target="_blank"
+            style={{ backgroundColor: "#2b2a2a" }}
+          >
+            <SiMedium size={"2em"} color={"white"} />
+          </a>
+          <a
+            href="https://www.instagram.com/wander_acg/"
+            target="_blank"
+            style={{ backgroundColor: "#2b2a2a" }}
+          >
+            <GrInstagram size={"2em"} color={"white"} />
+          </a>
+          <a
+            href="https://twitter.com/wanderklk1"
+            target="_blank"
+            style={{ backgroundColor: "#2b2a2a" }}
+          >
+            <GrTwitter size={"2em"} color={"white"} />
+          </a>
         </div>
       </div>
       <div>
