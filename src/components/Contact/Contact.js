052071d112288cce93c3dd09cbf1fb3d6050d686
wandergithub <wandergonzalez0@hotmail.com>
2023-03-24 14:45:08 -0400

    style contact me information

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,10 +6,18 @@ import { SlCallIn } from "react-icons/sl";
 import { BsGithub } from "react-icons/bs";
 import { SiMedium } from "react-icons/si";
 import { GrInstagram, GrTwitter } from "react-icons/gr";
+import "./Contact.scss";
 
 const Contact = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: "0px 20px"
+      }}
+    >
       <div
         className="title-section text-left text-sm-center aos-init aos-animate"
         data-aos="fade-up"
@@ -21,72 +29,74 @@ const Contact = () => {
         </h1>
         <span className="title-bg">Contact</span>
       </div>
-      <div>
-        <h2>Dont be shy!</h2>
-        <p>
-          Feel free to get in touch with me. I am always open to discussing new
-          projects, creative ideas or opportunities to be part of your visions.
-        </p>
-        <div>
-          <FaMap size={"2em"} />
+
+      <div className="contact-info">
+        <div className="text-p">
+          <h2 className="text-uppercase title-2">Dont be shy!</h2>
+          <p>
+            Feel free to get in touch with me. I am always open to discussing
+            new projects, creative ideas or opportunities to be part of your
+            visions.
+          </p>
           <div>
-            <h4>address point</h4>
-            <p>123 Stree New York City , United States Of America 750065.</p>
+            <FaMap size={"2em"} color="#ffb400" className="contact-icon" />
+            <div>
+              <h4 className="text-uppercase no-m title-3">address point</h4>
+              <p className="no-m">123 Stree New York City , United States Of America 750065.</p>
+            </div>
+          </div>
+          <div>
+            <GoMailRead size={"2em"} color="#ffb400" className="contact-icon" />
+            <div>
+              <h4 className="text-uppercase no-m title-3">mail me</h4>
+              <p className="no-m">wandergonzalez0@hotmail.com</p>
+            </div>
+          </div>
+          <div>
+            <SlCallIn size={"2em"} color="#ffb400" className="contact-icon" />
+            <div>
+              <h4 className="text-uppercase no-m title-3">call me</h4>
+              <p className="no-m">+1 849 631 6566</p>
+            </div>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/wander-gonzalez/"
+              target="_blank"
+              style={{ backgroundColor: "#2b2a2a" }}
+            >
+              <FaLinkedinIn size={"2em"} color={"white"} />
+            </a>
+            <a
+              href="https://github.com/wandergithub"
+              target="_blank"
+              style={{ backgroundColor: "#2b2a2a" }}
+            >
+              <BsGithub size={"2em"} color={"white"} />
+            </a>
+            <a
+              href="https://medium.com/@wandergonzalez0"
+              target="_blank"
+              style={{ backgroundColor: "#2b2a2a" }}
+            >
+              <SiMedium size={"2em"} color={"white"} />
+            </a>
+            <a
+              href="https://www.instagram.com/wander_acg/"
+              target="_blank"
+              style={{ backgroundColor: "#2b2a2a" }}
+            >
+              <GrInstagram size={"2em"} color={"white"} />
+            </a>
+            <a
+              href="https://twitter.com/wanderklk1"
+              target="_blank"
+              style={{ backgroundColor: "#2b2a2a" }}
+            >
+              <GrTwitter size={"2em"} color={"white"} />
+            </a>
           </div>
         </div>
-        <div>
-          <GoMailRead size={"2em"} />
-          <div>
-            <h4>mail me</h4>
-            <p>wandergonzalez0@hotmail.com</p>
-          </div>
-        </div>
-        <div>
-          <SlCallIn size={"2em"} />
-          <div>
-            <h4>call me</h4>
-            <p>+1 849 631 6566</p>
-          </div>
-        </div>
-        <div>
-          <a
-            href="https://www.linkedin.com/in/wander-gonzalez/"
-            target="_blank"
-            style={{ backgroundColor: "#2b2a2a" }}
-          >
-            <FaLinkedinIn size={"2em"} color={"white"} />
-          </a>
-          <a
-            href="https://github.com/wandergithub"
-            target="_blank"
-            style={{ backgroundColor: "#2b2a2a" }}
-          >
-            <BsGithub size={"2em"} color={"white"} />
-          </a>
-          <a
-            href="https://medium.com/@wandergonzalez0"
-            target="_blank"
-            style={{ backgroundColor: "#2b2a2a" }}
-          >
-            <SiMedium size={"2em"} color={"white"} />
-          </a>
-          <a
-            href="https://www.instagram.com/wander_acg/"
-            target="_blank"
-            style={{ backgroundColor: "#2b2a2a" }}
-          >
-            <GrInstagram size={"2em"} color={"white"} />
-          </a>
-          <a
-            href="https://twitter.com/wanderklk1"
-            target="_blank"
-            style={{ backgroundColor: "#2b2a2a" }}
-          >
-            <GrTwitter size={"2em"} color={"white"} />
-          </a>
-        </div>
-      </div>
-      <div>
         <form>
           <input type={"text"} placeholder={"NAME"} />
           <input type={"text"} placeholder={"EMAIL"} />
@@ -98,7 +108,6 @@ const Contact = () => {
           />
         </form>
       </div>
-      <div style={{ alignSelf: "center", margin: "20px 0px" }}></div>
     </div>
   );
 };
