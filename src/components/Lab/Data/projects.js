8aa56ddc9c251ae8387a4a7dc22195e12dfc32c2
wandergithub <wandergonzalez0@hotmail.com>
2023-03-27 13:01:45 -0400

    Add Lab  content

--- src/components/Lab/Data/projects.js
+++ src/components/Lab/Data/projects.js
@@ -1,6 +1,6 @@
-import { TbBrandGithub } from "react-icons/tb";
 import { GoBrowser } from "react-icons/go";
 import image1 from "../../assets/img/blog-post-1.webp";
+import image2 from "../../assets/img/blog-post-2.png";
 
 const projects = [
   {
@@ -17,38 +17,18 @@ const projects = [
     ],
   },
   {
-    title: "E-commerce",
-    image: image1,
+    title: "Recursion",
+    type: "Article",
+    image: image2,
     description:
-      "Completely responsive E-commerce web page. build with React/Redux and MaterialUI-styled-components. This allows the user to navigate through different categories of items, filter them by category and add them to the shopping cart.",
+      "Recursion is a fundamental concept in computer science that can be intimidating for beginners. However, with a solid understanding of the basics and the right resources, it can become a powerful tool for solving complex problems.",
     links: [
       {
-        image: <TbBrandGithub title="Github icon" size={"2em"} />,
-        href: "https://github.com/wandergithub/e-commerce",
-      },
-      {
         image: <GoBrowser title="Live demo icon" size={"2em"} />,
-        href: "https://wandergithub.github.io/e-commerce/",
-      },
-    ],
-  },
-  {
-    title: "E-commerce",
-    image: image1,
-    description:
-      "Completely responsive E-commerce web page. build with React/Redux and MaterialUI-styled-components. This allows the user to navigate through different categories of items, filter them by category and add them to the shopping cart.",
-    links: [
-      {
-        image: <TbBrandGithub title="Github icon" size={"2em"} />,
-        href: "https://github.com/wandergithub/e-commerce",
-      },
-      {
-        image: <GoBrowser title="Live demo icon" size={"2em"} />,
-        href: "https://wandergithub.github.io/e-commerce/",
+        href: "https://medium.com/@wandergonzalez0/recursion-a-journey-from-basics-to-mastery-d5d4498d4f89",
       },
     ],
   },
 ];
-
 
 export default projects;
