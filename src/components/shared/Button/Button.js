1f805727414b67d2670885dd0aacd7248812fb59
wandergithub <wandergonzalez0@hotmail.com>
2023-03-27 11:27:55 -0400

    Add button type propr to Button component

--- src/components/shared/Button/Button.js
+++ src/components/shared/Button/Button.js
@@ -1,9 +1,9 @@
 import "./Button.scss";
 
 const Button = (props) => {
-    const { name, iconComponent } = props;
+  const { name, iconComponent, type } = props;
   return (
-    <button className="button" style={{ margin: "20px 0px" }}>
+    <button type={type} className="button" style={{ margin: "20px 0px" }}>
       <span className="button-text">{name}</span>
       <span className="button-icon">{iconComponent}</span>
     </button>
