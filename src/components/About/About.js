d481176d519ad513dff4f460bef2580ca7f9c4e9
wandergithub <wandergonzalez0@hotmail.com>
2023-03-31 13:15:31 -0400

    Update about me paragraph

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,5 @@
 import './About.scss';
+import { Link } from 'react-router-dom';
 import { GrDocumentUser } from 'react-icons/gr';
 import me from './assets/me.webp';
 import FloatingSocial from '../FloatingSocial/FloatingSocial';
@@ -24,12 +25,19 @@ const About = () => (
           <span>Web Developer</span>
         </h1>
         <p className="text-p">
-          I am a Full-Stack web developer with a passion for computer science.
-          I graduated from college with a degree in Mechatronics, where I
-          developed an interest in electronics and technology. In my free
-          time, I enjoy reading and exercising.
+          I am a full-stack web developer. I am an expert in persisting towards
+          my objectives and doing what&apos;s needed to go a step further. I am used
+          to facing difficult problems and new challenges every day. I love
+          technology and computer science. I am disciplined in my work, as I am
+          in exercising and developing mentally and fiscally every day. If you
+          want to work together, feel free to
+          <Link to="/contact" style={{ textDecoration: 'none', color: '#ffb400' }}> contact me!</Link>
         </p>
-        <a href="https://docs.google.com/document/d/1WpT8wFZMX6YKyg4YN3l6hYIXNqSo1xpdtLb_zSInrt8/edit?usp=sharing" target="_blank" rel="noreferrer">
+        <a
+          href="https://docs.google.com/document/d/1WpT8wFZMX6YKyg4YN3l6hYIXNqSo1xpdtLb_zSInrt8/edit?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Button
             name="Resume"
             type="button"
