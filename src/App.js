6762f6721f19717569768fa726039fe90b43299d
wandergithub <wandergonzalez0@hotmail.com>
2023-04-01 17:57:54 -0400

    Implement Routes transitions react transition group

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import React, { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, useLocation, Routes } from 'react-router-dom';
 import ReactGA from 'react-ga';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -7,6 +8,7 @@ import About from './components/About/About';
 import Work from './components/Work/Work';
 import Contact from './components/Contact/Contact';
 import Lab from './components/Lab/Lab';
+import './index.css';
 
 const style = {
   separator: {
@@ -15,6 +17,7 @@ const style = {
 };
 
 function App() {
+  const location = useLocation();
   const [isopen, setisopen] = useState(false);
   const toggle = () => {
     setisopen(!isopen);
@@ -30,12 +33,20 @@ function App() {
       <Navbar toggle={toggle} />
       <Sidebar isopen={isopen} toggle={toggle} />
       <div style={style.separator} />
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/lab" element={<Lab />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <SwitchTransition>
+        <CSSTransition
+          key={location.key}
+          timeout={500}
+          classNames="fade"
+        >
+          <Routes location={location}>
+            <Route path="/" element={<About />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/lab" element={<Lab />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </CSSTransition>
+      </SwitchTransition>
     </>
   );
 }
