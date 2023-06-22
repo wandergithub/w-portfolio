0aa1d20e30c5a923b8ebeff2e46abf4ddc504754
Wander Gonzalez <wandergonzalez0@hotmail.com>
2023-06-21 23:02:06 -0400

    style github icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Navbar.scss';
 import { Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
+import { BsGithub } from 'react-icons/bs';
 import { PropTypes } from 'prop-types';
 import navbarItems from '../shared/NavbarItems';
 
@@ -48,22 +49,13 @@ const Navbar = (props) => {
         ))}
       </div>
       <div className="icons" style={{ paddingRight: '10px' }}>
-        <a href="https://github.com/wandergithub" target="_blank" rel="noreferrer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="github-icon icon-tabler icon-tabler-brand-github"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            fill="#000"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
-          </svg>
+        <a
+          href="https://github.com/wandergithub"
+          target="_blank"
+          className="contact-icons github-icon"
+          rel="noreferrer"
+        >
+          <BsGithub size="1.3em" color="white" />
         </a>
         <div className="mobile-menu-icon">
           <FaBars onClick={toggle} />
